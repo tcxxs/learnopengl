1c1823daa83e027cc481bb19a82c673a8d4138e4
tcxxs <asdic.xxs@gmail.com>
2019-06-12 00:12:42 +0800

    先收集cmd，再渲染

--- source/render/scene.cpp
+++ source/render/scene.cpp
@@ -1,4 +1,5 @@
 #include "scene.hpp"
+#include "render/command.hpp"
 
 Scene::ptr Scene::create(const std::string& name) {
 	Scene::ptr scene = std::shared_ptr<Scene>(new Scene());
@@ -12,13 +13,6 @@ Scene::ptr Scene::create(const std::string& name) {
 
 	scene->addCamera(scene->_conf["camera"]);
 
-	const auto models = scene->_conf["models"];
-	if (models.IsDefined()) {
-		for (const auto& it: models) {
-			scene->addModel(it);
-		}
-	}
-
 	const auto lights = scene->_conf["lights"];
 	if (lights.IsDefined()) {
 		for (const auto& it: lights) {
@@ -26,11 +20,20 @@ Scene::ptr Scene::create(const std::string& name) {
 		}
 	}
 
+	const auto models = scene->_conf["models"];
+	for (const auto& it: models) {
+		scene->addModel(it);
+	}
+
+	const auto pass = scene->_conf["pass"];
+	for (const auto& it: pass) {
+		//scene->addPass(it);
+	}
+
 	return scene;
 }
 
 Scene::~Scene() {
-	
 }
 
 void Scene::addCamera(const Config::node& conf) {
@@ -41,17 +44,6 @@ void Scene::addCamera(const Config::node& conf) {
 	_cam->lookAt(pos, tar);
 }
 
-void Scene::addModel(const Config::node& conf) {
-	const ModelProto::ptr& proto = ModelProtoMgr::inst().req(conf["type"].as<std::string>());
-	if (!proto)
-		return;
-	const ModelInst::ptr& model = proto->instance(conf);
-	if (!model)
-		return;
-	
-	_models[conf["type"].as<std::string>()] = proto;
-}
-
 void Scene::addLight(const Config::node& conf) {
 	const LightProto::ptr& proto = LightProtoMgr::inst().req(conf["type"].as<std::string>());
 	if (!proto)
@@ -59,13 +51,105 @@ void Scene::addLight(const Config::node& conf) {
 	const LightInst::ptr& light = proto->instance(conf);
 	if (!light)
 		return;
-	
-	_lights[conf["type"].as<std::string>()] = proto;
+
+	_lights.push_back(light);
+}
+
+void Scene::addModel(const Config::node& conf) {
+	const ModelProto::ptr& proto = ModelProtoMgr::inst().req(conf["type"].as<std::string>());
+	if (!proto)
+		return;
+	const ModelInst::ptr& model = proto->instance(conf);
+	if (!model)
+		return;
+
+	_models.push_back(model);
+}
+
+void Scene::addPass(const Config::node& conf) {
+	conf["name"].as<std::string>();
+	const std::string& in = conf["in"].as<std::string>();
+	if (!in.empty()) {
+
+		Frame::ptr outf = Frame::create();
+		outf->attachTexture();
+		_frames[in] = outf;
+		
+	}
+	const std::string& out = conf["out"].as<std::string>();
+	if (!out.empty()) {
+		Frame::ptr outf = Frame::create();
+		outf->attachTexture();
+		_frames[out] = outf;
+	}
+	const std::string& modelsk = conf["models"].as<std::string>();
+	if (!modelsk.empty()) {
+	}
+
 }
 
 void Scene::draw() {
+	CommandQueue cmds;
 	for (auto& it: _models) {
-		const ModelProto::ptr& proto = it.second;
-		proto->draw(_cam, _lights);
+		it->draw(cmds);
 	}
+
+	for (auto& it: cmds) {
+		drawCommand(it);
+	}
+}
+
+void Scene::drawCommand(const Command& cmd) {
+	cmd.material->use();
+	const Shader::ptr& shader = cmd.material->getShader();
+	shader->setVars(cmd.attrs);
+
+	glUniformMatrix4fv(shader->getVar("view"), 1, GL_FALSE, glm::value_ptr(_cam->getView()));
+	glUniformMatrix4fv(shader->getVar("proj"), 1, GL_FALSE, glm::value_ptr(_cam->getProj()));
+	glUniformMatrix4fv(shader->getVar("model"), 1, GL_FALSE, glm::value_ptr(cmd.model));
+	shader->setVar("camera_pos", _cam->getPos());
+
+	int dirs{0}, points{0}, spots{0};
+	std::string light;
+	for (const auto& it: _lights) {
+		const LightProto::ptr proto = it->prototype();
+		const std::string& light_type = proto->getName();
+		if (light_type == "dir") {
+			light = string_format("dirs[%d]", dirs++);
+			shader->setVar(light + ".dir", it->getDir());
+			shader->setVar(light + ".ambient", proto->attrs.getAttr<glm::vec3>("ambient"));
+			shader->setVar(light + ".diffuse", proto->attrs.getAttr<glm::vec3>("diffuse"));
+			shader->setVar(light + ".specular", proto->attrs.getAttr<glm::vec3>("specular"));
+		}
+		else if (light_type == "point") {
+			light = string_format("points[%d]", points++);
+			shader->setVar(light + ".pos", it->getPos());
+			shader->setVar(light + ".ambient", proto->attrs.getAttr<glm::vec3>("ambient"));
+			shader->setVar(light + ".diffuse", proto->attrs.getAttr<glm::vec3>("diffuse"));
+			shader->setVar(light + ".specular", proto->attrs.getAttr<glm::vec3>("specular"));
+			shader->setVar(light + ".constant", proto->attrs.getAttr<float>("constant"));
+			shader->setVar(light + ".linear", proto->attrs.getAttr<float>("linear"));
+			shader->setVar(light + ".quadratic", proto->attrs.getAttr<float>("quadratic"));
+		}
+		else if (light_type == "spot") {
+			light = string_format("spots[%d]", spots++);
+			shader->setVar(light + ".pos", it->getPos());
+			shader->setVar(light + ".dir", it->getDir());
+			shader->setVar(light + ".ambient", proto->attrs.getAttr<glm::vec3>("ambient"));
+			shader->setVar(light + ".diffuse", proto->attrs.getAttr<glm::vec3>("diffuse"));
+			shader->setVar(light + ".specular", proto->attrs.getAttr<glm::vec3>("specular"));
+			shader->setVar(light + ".constant", proto->attrs.getAttr<float>("constant"));
+			shader->setVar(light + ".linear", proto->attrs.getAttr<float>("linear"));
+			shader->setVar(light + ".quadratic", proto->attrs.getAttr<float>("quadratic"));
+			shader->setVar(light + ".inner", cos(glm::radians(proto->attrs.getAttr<float>("inner"))));
+			shader->setVar(light + ".outter", cos(glm::radians(proto->attrs.getAttr<float>("outter"))));
+		}
+	}
+	shader->setVar("uses.dirs", dirs);
+	shader->setVar("uses.points", points);
+	shader->setVar("uses.spots", spots);
+
+	glBindVertexArray(cmd.vao);
+	glDrawElements(GL_TRIANGLES, cmd.ibosize, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
--- source/render/scene.hpp
+++ source/render/scene.hpp
@@ -6,6 +6,7 @@
 #include "render/camera.hpp"
 #include "render/model.hpp"
 #include "render/light.hpp"
+#include "render/frame.hpp"
 
 class Scene: public Res<Scene> {
 public:
@@ -14,21 +15,25 @@ public:
 
 	const Camera::ptr& getCamera() const { return _cam; }
 	void addCamera(const Config::node& conf);
-	void addModel(const Config::node& conf);
 	void addLight(const Config::node& conf);
+	void addModel(const Config::node& conf);
+	void addPass(const Config::node& conf);
 
 	inline void active() {
 		Scene::current = shared_from_this();
 	}
 	void draw();
+	void drawCommand(const Command& cmd);
 
 public:
 	inline static ptr current{};
+
 private:
 	Config _conf;
 	Camera::ptr _cam;
-	std::map<std::string, ModelProto::ptr> _models;
-	std::map<std::string, LightProto::ptr> _lights;
+	std::vector<LightInst::ptr> _lights;
+	std::vector<ModelInst::ptr> _models;
+	std::map<std::string, Frame::ptr> _frames;
 };
 
 using SceneMgr = ResMgr<Scene>;
