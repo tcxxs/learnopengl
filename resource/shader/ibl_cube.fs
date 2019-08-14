94d1007798b716a4cea6038632c7d7885cb95592
tcxxs <asdic.xxs@gmail.com>
2019-08-15 00:51:21 +0800

    计算ibl env卷积

--- resource/shader/ibl_cube.fs
+++ resource/shader/ibl_cube.fs
@@ -14,5 +14,7 @@ void main() {
     vec3 pos = normalize(vertex.pos);
     float u = atan(pos.z, pos.x) / (2 * MATH_PI) + 0.5;
     float v = asin(pos.y) / MATH_PI + 0.5;
-    color_out = vec4(texture(frame, vec2(u, v)).rgb, 1.0);
+
+    vec3 color = texture(frame, vec2(u, v)).rgb;
+    color_out = vec4(color, 1.0);
 }
--- resource/shader/ibl_env.fs
+++ resource/shader/ibl_env.fs
@@ -11,5 +11,26 @@ uniform samplerCube cube;
 out vec4 color_out;
 
 void main() {
-    color_out = vec4(texture(cube, vertex.pos).rgb, 1.0);
+    vec3 normal = normalize(vertex.pos);
+    vec3 irradiance = vec3(0.0);
+
+    vec3 up = vec3(0.0, 1.0, 0.0);
+    vec3 right = cross(up, normal);
+    up = cross(normal, right);
+
+    float step = 0.025;
+    int num = 0;
+    for (float phi = 0.0; phi < 2.0 * MATH_PI; phi += step) {
+        for (float theta = 0.0; theta < 0.5 * MATH_PI; theta += step) {
+            num++;
+            float sint = sin(theta);
+            float cost = cos(theta);
+            vec3 tangent = vec3(sint * cos(phi),  sint * sin(phi), cost);
+            vec3 world = mat3(right, up, normal) * tangent;
+            irradiance += texture(cube, world).rgb * cost * sint;
+        }
+    }
+    irradiance = (MATH_PI / float(num)) * irradiance;
+
+    color_out = vec4(irradiance, 1.0);
 }
